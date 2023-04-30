ed3060f16c89e79bf0ffa3302102b2eabed16f93
E.Delalande <116553314+rat-balls@users.noreply.github.com>
2023-04-30 17:45:20 +0200

    added slime bar and slime mechanic

--- Assets/Scripts/CharacterInfo.cs
+++ Assets/Scripts/CharacterInfo.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 
 public class CharacterInfo : MonoBehaviour
-{
+{   
+    private const float MAX_SLIME = 100f;
     public OverlayTile activeTile;
+    public float SlimeReserve = MAX_SLIME;
+
+    [SerializeField] private Image slimeBar;
+
+    private void Update() {
+        slimeBar.fillAmount = SlimeReserve / MAX_SLIME;
+    }
+
 }
